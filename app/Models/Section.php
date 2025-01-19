93f2fa253132635b79843725d05d0399877efb9c
iftikharali72232 <iftikharali72232@gmail.com>
2025-01-19 19:39:25 +0500

    ...

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Section extends Model
 {
     use HasFactory;
-
+    protected $table = "sections";
     protected $fillable = ['name', 'shows_to', 'image_path'];
 
     public function questions()
     {
-        return $this->hasMany(SectionQuestion::class);
+        return $this->hasMany(SectionQuestion::class, 'id');
     }
 }
--- app/Models/SectionQuestion.php
+++ app/Models/SectionQuestion.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class SectionQuestion extends Model
 {
     use HasFactory;
-
+    protected $table = "section_questions";
     protected $fillable = ['section_id', 'question', 'answer1', 'answer2', 'answer3'];
 }
