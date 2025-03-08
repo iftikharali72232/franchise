0b9add4acdd1fa5cac45a5f52107ba01766214f9
iftikharali72232 <iftikharali72232@gmail.com>
2025-03-08 20:21:05 +0500

    ...

--- resources/views/layouts/panel/sidebar.blade.php
+++ resources/views/layouts/panel/sidebar.blade.php
@@ -47,7 +47,7 @@
                     <span class="ps-4">{{ trans('lang.sections') }}</span>
                 </a>
 
-                <a href="#" class="w-full flex text-white hover:text-gray-700 items-center hover:bg-white rounded-xl px-4 py-3 focus:outline-none group">
+                <a href="/profile" class="w-full flex text-white hover:text-gray-700 items-center hover:bg-white rounded-xl px-4 py-3 focus:outline-none group">
                     <img src="{{ asset('images/gear.png') }}" alt="settings" class="w-5 h-5 filter group-hover:invert group-hover:brightness-100">
                     <span class="ps-4">{{ trans('lang.settings') }}</span>
                 </a>
